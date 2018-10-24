f0088327faaa0b52589ef39f86868ec8de7a70a9
David Grudl <david@grudl.com>
2018-10-24 02:06:56 +0200

    Compiler: added optional 'external' as alias for 'dynamic'

--- src/DI/Compiler.php
+++ src/DI/Compiler.php
@@ -368,7 +368,7 @@ class Compiler
 			unset($config['create']);
 		}
 
-		$known = ['type', 'class', 'factory', 'arguments', 'setup', 'autowired', 'dynamic', 'inject', 'parameters', 'implement', 'run', 'tags', 'alteration'];
+		$known = ['type', 'class', 'factory', 'arguments', 'setup', 'autowired', 'dynamic', 'external', 'inject', 'parameters', 'implement', 'run', 'tags', 'alteration'];
 		if ($error = array_diff(array_keys($config), $known)) {
 			$hints = array_filter(array_map(function ($error) use ($known) {
 				return Nette\Utils\ObjectHelpers::getSuggestion($known, $error);
@@ -442,6 +442,10 @@ class Compiler
 		if (isset($config['autowired'])) {
 			Validators::assertField($config, 'autowired', 'bool|string|array');
 			$definition->setAutowired($config['autowired']);
+		}
+
+		if (isset($config['external'])) {
+			$config['dynamic'] = $config['external'];
 		}
 
 		if (isset($config['dynamic'])) {
